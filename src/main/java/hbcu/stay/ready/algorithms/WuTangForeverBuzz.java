50d0186ba5be91680ca22ac4b8b5803748c49dc2
Samuel Ghebremedhin <samuelka2001@yahoo.com>
2020-11-16 11:45:28 -0500

    Completed WuTangForever

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -1,8 +1,29 @@
 package hbcu.stay.ready.algorithms;
 
+
+
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        String str = "";
+        int n = 15;
+        for (int i = 1; i <= n;  i++) {
+
+            if (i % 3 == 0 && i % 5 == 0 ) {
+                str += "WuTang Forever";
+                str += "\n";
+            }else if(i % 3 == 0 ) {
+                str += "Wu";
+                str += "\n";
+            }else if (i % 5 == 0) {
+                str += "Tang";
+                str += "\n";
+            }else {
+                str += i;
+                str += "\n";
+            }
+         }
+        return str.substring(0,str.length() -1);
     }
-}
+ }
+
